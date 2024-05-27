a1e4ba8429b422a1782fe4030a4223682786542c
Immanuel <64789685+immanueligwe151@users.noreply.github.com>
2024-05-27 06:11:59 +0100

    reverted it to the original version to be ran from the local host

--- db.php
+++ db.php
@@ -1,10 +1,9 @@
 <?php
-$url = parse_url(getenv("mysql://b752a35a364057:b738e4bd@eu-cluster-west-01.k8s.cleardb.net/heroku_b4bc17926cf84d9?reconnect=true"));
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "instructions_db";
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = "heroku_b4bc17926cf84d9";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
